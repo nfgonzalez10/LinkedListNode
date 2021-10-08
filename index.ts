a648c05092eec40ae7112b4370ade025b513746b
nfgonzalez10 <nfgonzalez10@outlook.com>
2021-10-08 14:07:31 -0500

    Initial commit

---
+++ index.ts
@@ -0,0 +1,30 @@
+// Import stylesheets
+import './style.css';
+
+// Write TypeScript code!
+const appDiv: HTMLElement = document.getElementById('app');
+appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+
+class LinkedListNode{
+  data;
+  next;
+  constructor(data, nextNode?){
+    this.data = data;
+    this.next = nextNode;
+  }
+}
+
+var first = new LinkedListNode("a", new LinkedListNode("b", new LinkedListNode("c")))
+
+function insertNodeAfterData(first,newvalue, insertAfter){
+  if(!first?.data) return
+  if(first.data === insertAfter){
+    first.next = new LinkedListNode(newvalue,first.next)
+  } else {
+    insertNodeAfterData(first.next, newvalue,insertAfter)
+  }
+ 
+}
+
+insertNodeAfterData(first, "f", "b")
+console.log(first)
